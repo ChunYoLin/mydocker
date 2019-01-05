a82e72c5f3e5093ee9da7b0aed7bd0c8963ea03d
Chunyolin <jeff042099@gmail.com>
2019-01-05 15:09:29 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+}
